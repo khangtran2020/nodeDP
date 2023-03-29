859114cb5d68bbf6e57f9ec1f7a4fd29cc8205b6
Khang <kt36@njit.edu>
2023-03-28 20:38:40 -0400

    init

--- submit.sh
+++ submit.sh
@@ -12,6 +12,6 @@ for NS in 29.6 6.665 3.66 1.78 1.145 0.9295 0.815
 do
     for RUN in 1 2 3 4 5
     do
-        python main.py --folds 5 --seed $RUN --sampling_rate 0.05 --epochs 400 --lr 0.05 --clip 0.1 --ns $NS --clip_node 2 --dataset cora --mode dp
+        python main.py --folds 5 --seed $RUN --sampling_rate 0.05 --epochs 400 --lr 0.05 --clip 0.1 --ns $NS --clip_node 2 --dataset cora --mode dp --trim_rule random
     done
 done
