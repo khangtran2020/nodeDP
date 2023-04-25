82bb2a43222ddb026a630345bbd670b793c091c0
Khang <kt36@njit.edu>
2023-04-24 23:39:48 -0400

    init

--- Trim/trimming_rule.py
+++ Trim/trimming_rule.py
@@ -27,17 +27,18 @@ def impact_aware_trimming(roots, k, current_node, appear_dict=None, model=None, 
     if current_node in r: r.remove(current_node)
     args = [(r_, current_node, appear_dict, appear_dict_, model, graph) for r_ in r]
     with torch.no_grad():
-        print(f'Working with {os.cpu_count()} threads, for {len(r)} tasks')
-        with Pool(processes=os.cpu_count()) as pool:
-            results = [pool.apply(get_val, args=(arg,)) for arg in args]
-            smape = [res.get() for res in results]
+        # print(f'Working with {os.cpu_count()} threads, for {len(r)} tasks')
+        pool = Pool(processes=os.cpu_count())
+        results = pool.map(get_val, args)
+        pool.close()
+        smape = [res for res in results]
     root_rank = sorted(smape, key=lambda x: x[1])
     list_of_root = [x[0] for x in root_rank]
     return list_of_root[:k]
 
 def get_val(args):
     root, curr_node, appear_dict, appear_dict_, model, graph = args
-    print(f'working with root {root}')
+    # print(f'working with root {root}')
     appear_dict_.trim_node_from_root(node_id=curr_node, root=root)
     blocks = appear_dict.build_blocks(root=root, graph=graph)
     blocks_ = appear_dict_.build_blocks(root=root, graph=graph)
@@ -46,4 +47,4 @@ def get_val(args):
     predictions = model(blocks, inputs)
     predictions_ = model(blocks_, inputs_)
     val = torch.norm(predictions - predictions_, p=1) / (torch.norm(predictions, p=1) + torch.norm(predictions_, p=1))
-    return (root, val)
+    return (root, val.item())
