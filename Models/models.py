a9f54aa20fd0340e683b5c5e58ac99c99f1c4af5
Khang <kt36@njit.edu>
2023-07-03 01:55:52 -0400

    init

--- Models/models.py
+++ Models/models.py
@@ -6,31 +6,44 @@ from torch import nn
 class GraphSAGE(nn.Module):
     def __init__(self, in_feats, n_hidden, n_classes, n_layers, dropout=0.2, aggregator_type='gcn'):
         super().__init__()
-        self.layers = nn.ModuleList()
-        self.layers.append(dglnn.SAGEConv(in_feats, n_hidden, 'mean'))
-        for i in range(0, n_layers - 2):
-            self.layers.append(dglnn.SAGEConv(n_hidden, n_hidden, 'mean'))
-        self.layers.append(dglnn.SAGEConv(n_hidden, n_classes, 'mean'))
-        self.dropout = nn.Dropout(dropout)
         self.n_layers = n_layers
-        self.batch_norm = torch.nn.BatchNorm1d(n_hidden)
-        self.activation = torch.nn.ReLU()
-        self.last_activation = torch.nn.Softmax(dim=1) if n_classes > 1 else torch.nn.Sigmoid()
-        print(f"Using activation for last layer {self.last_activation}")
+        if n_layers > 1:
+            self.layers = nn.ModuleList()
+            self.layers.append(dglnn.SAGEConv(in_feats, n_hidden, 'mean'))
+            for i in range(0, n_layers - 2):
+                self.layers.append(dglnn.SAGEConv(n_hidden, n_hidden, 'mean'))
+            self.layers.append(dglnn.SAGEConv(n_hidden, n_classes, 'mean'))
+            self.dropout = nn.Dropout(dropout)
+            self.batch_norm = torch.nn.BatchNorm1d(n_hidden)
+            self.activation = torch.nn.SELU()
+            self.last_activation = torch.nn.Softmax(dim=1) if n_classes > 1 else torch.nn.Sigmoid()
+            print(f"Using activation for last layer {self.last_activation}")
+        else:
+            self.layer = dglnn.SAGEConv(in_feats, n_classes, 'mean')
+            self.dropout = nn.Dropout(dropout)
+            self.batch_norm = torch.nn.BatchNorm1d(n_hidden)
+            self.activation = torch.nn.SELU()
+            self.last_activation = torch.nn.Softmax(dim=1) if n_classes > 1 else torch.nn.Sigmoid()
+            print(f"Using activation for last layer {self.last_activation}")
 
     def forward(self, blocks, x):
-        h = x
-        for i in range(0, self.n_layers-1):
-            h_dst = h[:blocks[i].num_dst_nodes()]
-            h = self.layers[i](blocks[i], (h, h_dst))
-            # h = self.batch_norm(h)
-            h = self.dropout(h)
-            h = self.activation(h)
-            norm = h.norm(p=2, dim=1, keepdim = True)
-            h = h.div(norm + 1e-12)
-        h_dst = h[:blocks[-1].num_dst_nodes()]
-        h = self.last_activation(self.layers[-1](blocks[-1], (h, h_dst)))
-        return h
+        if self.n_layers > 1:
+            h = x
+            for i in range(0, self.n_layers-1):
+                h_dst = h[:blocks[i].num_dst_nodes()]
+                h = self.layers[i](blocks[i], (h, h_dst))
+                # h = self.batch_norm(h)
+                h = self.dropout(h)
+                h = self.activation(h)
+            h_dst = h[:blocks[-1].num_dst_nodes()]
+            h = self.last_activation(self.layers[-1](blocks[-1], (h, h_dst)))
+            return h
+        else:
+            h = x
+            h_dst = h[:blocks[0].num_dst_nodes()]
+            h = self.last_activation(self.layer(blocks[0], (h, h_dst)))
+            return h
+
 
 class GAT(nn.Module):
     def __init__(self, in_feats, n_hidden, n_classes, n_layers, num_head=8, dropout=0.2):
