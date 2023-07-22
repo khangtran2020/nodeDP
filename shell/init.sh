455e111bd06fa70624722e8ff093ba9efac8d069
Khang <kt36@njit.edu>
2023-07-22 16:58:42 -0400

    init

--- shell/init.sh
+++ shell/init.sh
@@ -3,6 +3,7 @@ mkdir results/dict
 mkdir results/models
 mkdir results/logs
 
+conda activate torch
 conda install pytorch==1.12.1 torchvision==0.13.1 torchaudio==0.12.1 cudatoolkit=11.3 -c pytorch -c nvidia -y
 conda install -c dglteam/label/cu113 dgl -y
 conda install pyg -c pyg -y
