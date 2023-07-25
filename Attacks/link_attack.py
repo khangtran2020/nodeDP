b97184fc32ebf4080240f17c7d3f3d143b94e11e
Khang <kt36@njit.edu>
2023-07-25 16:32:20 -0400

    init

--- Attacks/link_attack.py
+++ Attacks/link_attack.py
@@ -53,6 +53,6 @@ def run_LinkTeller(args, current_time, device):
             tar_model = init_model(args=args)
             tar_model.load_state_dict(torch.load(args.save_path + f'{args.tar_name}.pt'))
 
-    attack = Attacker(args=args, graph=train_g, model=tar_model, n_samples=500, influence=0.01, device=torch.device('cpu'))
+    attack = Attacker(args=args, graph=train_g, model=tar_model, n_samples=500, influence=0.01, device=device)
     attack.construct_edge_sets_from_random_subgraph()
     attack.link_prediction_attack_efficient()
