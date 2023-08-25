e69f02512b98adc235c3cdac0d5adb1d2a996423
Khang <kt36@njit.edu>
2023-08-25 12:11:05 -0400

    mlp

--- submit.sh
+++ submit.sh
@@ -2,7 +2,7 @@
 # The above line must always be first, and must have "-l"
 #SBATCH -J NodeDP
 #SBATCH -p datasci
-#SBATCH --output=results/logs/clean.out
+#SBATCH --output=results/logs/reddit_mlp.out
 #SBATCH --mem=64G
 #SBATCH --gres=gpu:0
 module load python
@@ -11,19 +11,42 @@ conda activate torch
 
 # mlp submission
 
-python main.py --mode mlp \
-        --mlp_mode clean \
-        --dataset facebook \
-        --model_type mlp \
-        --lr 0.01 \
-        --sampling_rate 0.01 \
-        --n_layers 2 \
-        --hid_dim 16 \
-        --epochs 200 \
-        --clip 1.0 \
-        --ns 0.622 \
-        --debug 0 \
-        --n_neighbor 3 \
-        --seed 1 \
-        --dropout 0.0 \
-        --device gpu
+for RUN in 1 2 3 4 5
+do 
+	python main.py --mode mlp \
+		--mlp_mode clean \
+		--dataset reddit \
+		--model_type mlp \
+		--lr 0.01 \
+		--sampling_rate 0.01 \
+		--n_layers 2 \
+		--hid_dim 16 \
+		--epochs 200 \
+		--clip 1.0 \
+		--ns 0.622 \
+		--debug 0 \
+		--n_neighbor 3 \
+		--seed $RUN \
+		--dropout 0.0 \
+		--device gpu
+done
+
+for RUN in 1 2 3 4 5
+do 
+	python main.py --mode mlp \
+		--mlp_mode dp \
+		--dataset reddit \
+		--model_type mlp \
+		--lr 0.01 \
+		--sampling_rate 0.01 \
+		--n_layers 2 \
+		--hid_dim 16 \
+		--epochs 200 \
+		--clip 1.0 \
+		--ns 0.622 \
+		--debug 0 \
+		--n_neighbor 3 \
+		--seed $RUN \
+		--dropout 0.0 \
+		--device gpu
+done
