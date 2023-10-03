e14996c47d5ca60e37986180f518c807ecd707cd
Khang <kt36@njit.edu>
2023-10-03 15:12:54 +0300

    modifying whitebox

--- Attacks/Utils/dataset.py
+++ Attacks/Utils/dataset.py
@@ -105,7 +105,7 @@ def custom_collate(batch, out_key, model_key, device, num_class):
             grad = torch.unsqueeze(it_grad_dict[key], dim=0).detach()
             grad_dict[key] = torch.cat((grad_dict[key], grad), dim=0)
 
-    label = F.one_hot(label.int(), num_class).float().to(device)
+    label = F.one_hot(label.squeeze().int(), num_class).float().to(device)
 
     return (label, loss, out_dict, grad_dict), membership_label
 
