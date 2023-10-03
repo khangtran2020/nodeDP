6ee71366c09e8110235cdb25eebdd876ff2b9f6d
Khang <kt36@njit.edu>
2023-10-03 15:05:42 +0300

    modifying whitebox

--- Attacks/Runs/white_box.py
+++ Attacks/Runs/white_box.py
@@ -6,7 +6,7 @@ from hashlib import md5
 from rich import print as rprint
 from rich.pretty import pretty_repr
 from Utils.utils import timeit
-from Models.models import NN, CustomNN
+from Models.models import WbAttacker
 from Models.init import init_model, init_optimizer
 from Attacks.Utils.utils import save_dict
 from Attacks.Utils.dataset import Data, ShadowData, custom_collate
@@ -43,25 +43,40 @@ def run(args, graph, model, device, history, name):
         for named, p in model.named_parameters():
             if p.requires_grad:
                 model_keys.append(named)
+                rprint(f"Model parameter {named} has size: {p.tensor.size()}")
         
-        collate_fn = partial(custom_collate, out_key=out_keys, model_key=model_keys, device=device)
+        collate_fn = partial(custom_collate, out_key=out_keys, model_key=model_keys, device=device, num_class=args.num_class)
         tr_loader = torch.utils.data.DataLoader(shtr_dataset, batch_size=args.att_bs, collate_fn=collate_fn,
                                                 drop_last=True, shuffle=True)
         te_loader = torch.utils.data.DataLoader(shte_dataset, batch_size=args.att_bs, collate_fn=collate_fn,
                                                 drop_last=False, shuffle=True)
+        
+        x, y = next(iter(tr_loader))
+        label, loss, out_dict, grad_dict = x
+        rprint(f"Label: {label}, size: {label.size()}")
+        rprint(f"Loss: {loss}, size: {loss.size()}")
+        rprint(f"Membership Label: {y}, size: {y.size()}")
+        for key in out_keys:
+            rprint(f"Out dict at key {key} has size: {out_dict[key].size()}")
+        for key in model_keys:
+            rprint(f"Grad dict at key {key} has size: {grad_dict[key].size()}")
+        sys.exit()
+        
 
     # device = torch.device('cpu')
     with timeit(logger=logger, task='train-attack-model'):
+        
+        att_model = WbAttacker(label_dim=args.num_class, loss_dim=1, )
 
-        attack_model = NN(input_dim=new_dim, hidden_dim=64, output_dim=1, n_layer=3)
-        attack_optimizer = init_optimizer(optimizer_name=args.optimizer, model=attack_model, lr=args.lr)
+    #     attack_model = NN(input_dim=new_dim, hidden_dim=64, output_dim=1, n_layer=3)
+    #     attack_optimizer = init_optimizer(optimizer_name=args.optimizer, model=attack_model, lr=args.lr)
 
-        attack_model = train_attack(args=args, tr_loader=tr_loader, va_loader=va_loader, te_loader=te_loader,
-                                    attack_model=attack_model, epochs=args.attack_epochs, optimizer=attack_optimizer,
-                                    name=tar_history['name'], device=device)
+    #     attack_model = train_attack(args=args, tr_loader=tr_loader, va_loader=va_loader, te_loader=te_loader,
+    #                                 attack_model=attack_model, epochs=args.attack_epochs, optimizer=attack_optimizer,
+    #                                 name=tar_history['name'], device=device)
 
-    attack_model.load_state_dict(torch.load(args.save_path + f"{tar_history['name']}_attack.pt"))
-    te_loss, te_auc = eval_attack_step(model=attack_model, device=device, loader=te_loader,
-                                       metrics=torchmetrics.classification.BinaryAUROC().to(device),
-                                       criterion=torch.nn.BCELoss())
-    rprint(f"Attack AUC: {te_auc}")
+    # attack_model.load_state_dict(torch.load(args.save_path + f"{tar_history['name']}_attack.pt"))
+    # te_loss, te_auc = eval_attack_step(model=attack_model, device=device, loader=te_loader,
+    #                                    metrics=torchmetrics.classification.BinaryAUROC().to(device),
+    #                                    criterion=torch.nn.BCELoss())
+    # rprint(f"Attack AUC: {te_auc}")
