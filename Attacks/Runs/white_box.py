5ab2474d8be7988e3fcee1541c5e8191df197847
Khang <kt36@njit.edu>
2023-10-03 12:23:50 +0300

    modifying whitebox

--- Attacks/Runs/white_box.py
+++ Attacks/Runs/white_box.py
@@ -39,14 +39,15 @@ def run(args, graph, model, device, history, name):
         shte_dataset = ShadowData(graph=shadow_graph, model=model, num_layer=args.n_layers, device=device, mode='test')
         
         out_keys = [f'out_{i}' for i in range(args.n_layers)]
+        grad_keys = []
+        for named, p in model.named_parameters():
+            if p.requires_grad:
+                grad_keys.append(named)
+                
         x, y = shtr_dataset.__getitem__(index=0)
         it_loss, it_label, it_out_dict, it_grad_dict = x
-
-        rprint(f"Loss: {it_loss}, size: {it_loss.size()}")
-        rprint(f"Label: {it_label}, size: {it_label.size()}")
-        rprint(f"Membership Label: {y}, size: {y.size()}")
-        rprint(f"Out dict keys: {it_out_dict.keys()}")
-        rprint(f"Grad dict keys: {it_grad_dict.keys()}")
+        rprint(f"Out dict keys: {it_out_dict.keys()}, out_keys: {out_keys}")
+        rprint(f"Grad dict keys: {it_grad_dict.keys()}, grad_keys: {grad_keys}")
         sys.exit()
 
     # device = torch.device('cpu')
