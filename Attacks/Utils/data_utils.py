0d810e492377868479b0c4465fec475d7ca3f39b
Khang <kt36@njit.edu>
2023-10-07 10:44:10 +0300

    test no training

--- Attacks/Utils/data_utils.py
+++ Attacks/Utils/data_utils.py
@@ -463,6 +463,8 @@ def read_data(args, history, exist=False):
     id_intr = g_train.ndata['org_id']
     graph.ndata['id_intr'] = (torch.zeros(graph.nodes().size(dim=0)) - 1).int()
     graph.ndata['id_intr'][id_intr] = g_train.nodes().clone().int()
+    index = get_index_by_value(a=graph.ndata['train_mask'], val=1)
+    rprint(f"Distribution of id_intr in org: {graph.ndata['id_intr'][index].unique(return_counts=True)}")
 
     id_inte = g_test.ndata['org_id']
     graph.ndata['id_inte'] = (torch.zeros(graph.nodes().size(dim=0)) - 1).int()
