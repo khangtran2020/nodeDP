9e879898943628976f0763786cd483d37ecdc928
Khang <kt36@njit.edu>
2023-10-08 10:56:05 +0300

    test grad on self

--- Attacks/Utils/train_eval.py
+++ Attacks/Utils/train_eval.py
@@ -107,7 +107,7 @@ def train_attack(args, tr_loader, va_loader, te_loader, attack_model, epochs, op
         te_loss, te_acc = eval_attack_step(model=attack_model, device=device, loader=te_loader, metrics=metrics,
                                            criterion=criterion)
         
-        rprint(f"At epoch {epoch}: tr_loss {tr_loss}, tr_acc {tr_acc}, va_loss {va_loss}, va_acc {va_acc}")
+        # rprint(f"At epoch {epoch}: tr_loss {tr_loss}, tr_acc {tr_acc}, va_loss {va_loss}, va_acc {va_acc}")
 
         history['attr_loss'].append(tr_loss)
         history['attr_perf'].append(tr_acc)
