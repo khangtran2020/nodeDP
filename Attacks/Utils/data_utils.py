e0264f34cf359bbad2f760159919afd963152f3d
Khang <kt36@njit.edu>
2023-10-08 16:16:12 +0300

    modifying subgraph

--- Attacks/Utils/data_utils.py
+++ Attacks/Utils/data_utils.py
@@ -680,8 +680,8 @@ def shadow_split_whitebox_subgraph(graph, tr_graph, te_graph, n_layer, max_nei,
 
         id_intr = graph.ndata['id_intr'][shapos_nodes]
         id_inte = graph.ndata['id_inte'][shaneg_nodes]
-        sys.exit("To line 683")
         block_pos = sample_blocks(graph=tr_graph, nodes=id_intr, n_layer=n_layer, max_nei=max_nei)
+        sys.exit("To line 684")
         block_neg = sample_blocks(graph=te_graph, nodes=id_inte, n_layer=n_layer, max_nei=max_nei)
 
         src_edge = torch.Tensor([])
