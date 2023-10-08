ee013449b5cf2bca0a9e01e8f499cff0b8296d5e
Khang <kt36@njit.edu>
2023-10-08 08:56:35 +0300

    test grad on self

--- Attacks/Utils/train_eval.py
+++ Attacks/Utils/train_eval.py
@@ -307,7 +307,7 @@ def get_grad(shadow_graph, target_graph, model, criterion, device, mask, pos=Fal
                 grad_tr = torch.Tensor([]).to(device)
                 loss_tr[id_tr].backward(retain_graph=True)
                 for name, p in model.named_parameters():
-                    if p.grad is not None:
+                    if (p.grad is not None) & ('fc_self' in name):
                         new_grad = p.grad.detach().clone()
                         grad_tr = torch.cat((grad_tr, new_grad.flatten()), dim=0)
                 model.zero_grad()
