1d50155c859ac956c9ed9d9320d51f791ae6c606
Khang <kt36@njit.edu>
2023-10-08 09:37:59 +0300

    test grad on self

--- Attacks/Utils/data_utils.py
+++ Attacks/Utils/data_utils.py
@@ -333,7 +333,7 @@ def generate_attack_samples(graph, conf, nohop_conf, mode, device, te_graph=None
 def generate_nohop_graph(graph, device):
     
     nodes = graph.nodes().tolist()
-    g = dgl.graph((nodes, nodes), num_nodes=len(nodes)).to(device)
+    g = dgl.graph(([], []), num_nodes=len(nodes)).to(device)
     for key in graph.ndata.keys():
         g.ndata[key] = graph.ndata[key].clone()
     
