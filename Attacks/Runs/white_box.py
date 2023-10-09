c7de8dbd3eb0f116772bba1a3b36628575e4a2f7
Khang <kt36@njit.edu>
2023-10-09 19:49:32 +0300

    fixing eval

--- Attacks/Runs/white_box.py
+++ Attacks/Runs/white_box.py
@@ -35,11 +35,11 @@ def run(args, graph, model, device, history, name):
     with timeit(logger=logger, task='preparing-shadow-data'):
         
         if args.att_submode == 'drop':
-            shtr_dataset = ShadowData(graph=shadow_graph, model=model, num_layer=args.n_layers, device=device, mode='train', weight='drop')
-            shte_dataset = ShadowData(graph=shadow_graph, model=model, num_layer=args.n_layers, device=device, mode='test', weight='drop')
+            shtr_dataset = ShadowData(graph=shadow_graph, model=model, num_layer=args.n_layers, device=device, mode='train', weight='drop', nnei=4)
+            shte_dataset = ShadowData(graph=shadow_graph, model=model, num_layer=args.n_layers, device=device, mode='test', weight='drop', nnei=4)
         else:
-            shtr_dataset = ShadowData(graph=shadow_graph, model=model, num_layer=args.n_layers, device=device, mode='train', nnei=2)
-            shte_dataset = ShadowData(graph=shadow_graph, model=model, num_layer=args.n_layers, device=device, mode='test', nnei=2)
+            shtr_dataset = ShadowData(graph=shadow_graph, model=model, num_layer=args.n_layers, device=device, mode='train')
+            shte_dataset = ShadowData(graph=shadow_graph, model=model, num_layer=args.n_layers, device=device, mode='test')
 
         label, weight = shtr_dataset.label_weight
         lab_weight = 1 - weight / weight.sum()
