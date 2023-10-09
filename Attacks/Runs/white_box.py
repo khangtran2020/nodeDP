9a6ea5859d6144521b7f38d602763d8fa0e83088
Khang <kt36@njit.edu>
2023-10-09 16:23:38 +0300

    fixing eval

--- Attacks/Runs/white_box.py
+++ Attacks/Runs/white_box.py
@@ -26,9 +26,8 @@ def run(args, graph, model, device, history, name):
         if args.exist_model == False:
             rprint(f"Model is {args.exist_model} to exist, need to retrain")
             model_name = f"{md5(name['model'].encode()).hexdigest()}.pt"
-            model, model_hist = retrain(args=args, train_g=train_g, val_g=val_g, test_g=test_g, model=model, 
-                                        device=device, history=model_hist, name=model_name[:-3])
-            
+            # model, model_hist = retrain(args=args, train_g=train_g, val_g=val_g, test_g=test_g, model=model, 
+            #                             device=device, history=model_hist, name=model_name[:-3])      
             target_model_name = f"{md5(name['model'].encode()).hexdigest()}.pkl"
             target_model_path = args.res_path + target_model_name
             save_dict(path=target_model_path, dct=model_hist)
