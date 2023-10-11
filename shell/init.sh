80e244aa6c3e0d10679144f03fbae9ef5d9adbf4
Khang <kt36@njit.edu>
2023-10-11 11:58:55 +0300

    fixing eval

--- shell/init.sh
+++ shell/init.sh
@@ -5,6 +5,7 @@ mkdir results/logs
 mkdir Data/pairs
 mkdir Data/features
 
+pip install wandb
 pip install torch==1.12.1+cu113 torchvision==0.13.1+cu113 torchaudio==0.12.1 --extra-index-url https://download.pytorch.org/whl/cu113
 pip install  dgl -f https://data.dgl.ai/wheels/cu116/repo.html
 pip install  dglgo -f https://data.dgl.ai/wheels-test/repo.html
